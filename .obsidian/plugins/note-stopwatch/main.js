d5e59f29719bf11b24f3e5a1ec9421668108dda5
ssyeu <syeu3251@gmail.com>
2026-09-23 15:50:57 -0500

    vault backup: 2026-09-23 15:50:57

--- .obsidian/plugins/note-stopwatch/main.js
+++ .obsidian/plugins/note-stopwatch/main.js
@@ -1,7 +1,26 @@
-const { Plugin, Notice } = require("obsidian");
+const { Plugin, Notice, ItemView } = require("obsidian");
 
 module.exports = class NoteStopwatch extends Plugin {
   onload() {
     new Notice("Note Stopwatch loaded!");
   }
 };
+
+class StopwatchView extends ItemView {
+  getViewType() {
+    return "note-stopwatch-view";
+  }
+
+  getDisplayText() {
+    return "Stopwatch";
+  }
+
+  getIcon() {
+    return "timer";
+  }
+
+  async onOpen() {
+    this.contentEl.empty();
+    this.contentEl.createEl("h2", { text: "00:00:00" });
+  }
+}
