6ef03e8b0bee77611320c09e6ec40d16acf0a5f4
ssyeu <syeu3251@gmail.com>
2026-09-23 15:53:39 -0500

    vault backup: 2026-09-23 15:53:39

--- .obsidian/plugins/note-stopwatch/main.js
+++ .obsidian/plugins/note-stopwatch/main.js
@@ -1,27 +1,31 @@
 const { Plugin, Notice, ItemView } = require("obsidian");
 
 module.exports = class NoteStopwatch extends Plugin {
-    onload() {
-    this.registerView(
-      "note-stopwatch-view",
-      (leaf) => new StopwatchView(leaf)
-    );
+      async onOpen() {
+    this.contentEl.empty();
 
-    this.addRibbonIcon("timer", "Open stopwatch", async () => {
-      let leaf = this.app.workspace
-        .getLeavesOfType("note-stopwatch-view")[0];
-
-      if (!leaf) {
-        leaf = this.app.workspace.getLeftLeaf(true);
-        if (!leaf) return;
-
-        await leaf.setViewState({
-          type: "note-stopwatch-view"
-        });
-      }
-
-      await this.app.workspace.revealLeaf(leaf);
+    this.startedAt = Date.now();
+    this.timerEl = this.contentEl.createEl("h2", {
+      text: "00:00:00"
     });
+
+    this.registerInterval(
+      window.setInterval(() => {
+        const seconds = Math.floor(
+          (Date.now() - this.startedAt) / 1000
+        );
+
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const remainingSeconds = seconds % 60;
+
+        this.timerEl.setText(
+          [hours, minutes, remainingSeconds]
+            .map(number => String(number).padStart(2, "0"))
+            .join(":")
+        );
+      }, 250)
+    );
   }
 };
 
