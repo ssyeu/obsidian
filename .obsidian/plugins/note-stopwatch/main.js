c845e6ffada6dcf5a47d50c1f42b2a1f6efd0236
ssyeu <syeu3251@gmail.com>
2026-09-23 15:55:36 -0500

    vault backup: 2026-09-23 15:55:36

--- .obsidian/plugins/note-stopwatch/main.js
+++ .obsidian/plugins/note-stopwatch/main.js
@@ -1,31 +1,27 @@
-const { Plugin, Notice, ItemView } = require("obsidian");
+const { Plugin, ItemView } = require("obsidian");
 
 module.exports = class NoteStopwatch extends Plugin {
-      async onOpen() {
-    this.contentEl.empty();
-
-    this.startedAt = Date.now();
-    this.timerEl = this.contentEl.createEl("h2", {
-      text: "00:00:00"
-    });
-
-    this.registerInterval(
-      window.setInterval(() => {
-        const seconds = Math.floor(
-          (Date.now() - this.startedAt) / 1000
-        );
-
-        const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds % 3600) / 60);
-        const remainingSeconds = seconds % 60;
-
-        this.timerEl.setText(
-          [hours, minutes, remainingSeconds]
-            .map(number => String(number).padStart(2, "0"))
-            .join(":")
-        );
-      }, 250)
+  onload() {
+    this.registerView(
+      "note-stopwatch-view",
+      (leaf) => new StopwatchView(leaf)
     );
+
+    this.addRibbonIcon("timer", "Open stopwatch", async () => {
+      let leaf = this.app.workspace
+        .getLeavesOfType("note-stopwatch-view")[0];
+
+      if (!leaf) {
+        leaf = this.app.workspace.getLeftLeaf(true);
+        if (!leaf) return;
+
+        await leaf.setViewState({
+          type: "note-stopwatch-view"
+        });
+      }
+
+      await this.app.workspace.revealLeaf(leaf);
+    });
   }
 };
 
@@ -44,6 +40,33 @@ class StopwatchView extends ItemView {
 
   async onOpen() {
     this.contentEl.empty();
-    this.contentEl.createEl("h2", { text: "00:00:00" });
+
+    this.startedAt = Date.now();
+
+    this.timerEl = this.contentEl.createEl("h2", {
+      text: "00:00:00"
+    });
+
+    this.intervalId = window.setInterval(() => {
+      const seconds = Math.floor(
+        (Date.now() - this.startedAt) / 1000
+      );
+
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const remainingSeconds = seconds % 60;
+
+      this.timerEl.setText(
+        [hours, minutes, remainingSeconds]
+          .map(number => String(number).padStart(2, "0"))
+          .join(":")
+      );
+    }, 250);
+
+    this.registerInterval(this.intervalId);
+  }
+
+  async onClose() {
+    window.clearInterval(this.intervalId);
   }
 }
